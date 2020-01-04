1ef1caccd7688988e4760287cc11356406d0a72e
vlakreeh <riverserver2012@gmail.com>
2020-01-04 07:43:58 -0500

    Start discord integration

---
+++ src/discord/bot.ts
@@ -0,0 +1,41 @@
+import { Client, MessageReaction, User, Message } from "discord.js";
+import { Submission } from "../reddit";
+
+export default class DiscordBot {
+
+    private client: Client;
+
+    constructor(client: Client) {
+        this.client = client;
+        this.client.on("messageReactionAdd", this.onReactionAdd);
+        this.client.on("messageReactionRemove", this.onReactionAdd);
+        this.client.on("message", this.onMessage);
+    }
+
+    // Handles registering users to a category by adding a reaction to
+    // an emoji that signifies a category.
+    onReactionAdd(messageReaction: MessageReaction, user: User) {
+
+    }
+
+    onReactionRemove(messageReaction: MessageReaction, user: User) {
+
+    }
+
+    onMessage(message: Message) {
+        if (message.content === "!setup") { // TODO: Setup permissions for initial bot setup.
+            // TODO: Set up channels and stuff.
+        }
+    }
+
+    /**
+     * Sends a notification to a Discord channel letting users know that
+     * there is a new item that is on sale.
+     * 
+     * @param submission The submission to notify the users about.
+     */
+    sendSubmissionAlert(submission: Submission) {
+
+    }
+
+}
