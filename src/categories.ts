728c2bf2c1e8d246980dfab6f35e71c28fbbffe9
vlakreeh <riverserver2012@gmail.com>
2020-01-05 09:30:17 -0500

    Implement basic category detection

--- src/categories.ts
+++ src/categories.ts
@@ -13,10 +13,38 @@ export enum Category {
     Psu = "Psu",
     Ram = "Ram",
     Ssd = "Ssd",
-    Mobo = "Mobo",
+    Motherboard = "Motherboard",
     Prebuilt = "Prebuilt",
-    VR = "VR"
+    VR = "VR",
+    Other = "Other"
 }
 
 // This is apparently the cleanest way to get all of the values of an enum in TypeScript.
 export const CATEGORIES: Category[] = Object.keys(Category).map(key => (Category as any)[key]);
+
+// Not everyone on the subreddit follows the conventions for categories,
+// so here are some commonly used non-official categories mapped to their
+// official counterparts.
+const CATEGORY_MAPPINGS: Map<String, Category> = new Map([
+    ["case fan", Category.Fan],
+    ["graphics card", Category.Gpu],
+    ["aio", Category.Cooler],
+    ["cpu cooler", Category.Cooler],
+    ["mobo", Category.Motherboard],
+    ["nvme", Category.Ssd],
+]);
+
+export function getCategoryForTitle(title: string): Category {
+    const matches = title.match(/\[(.+)\].+/);
+
+    // Sometimes people don't provide the category of a product, so we will default to the "other" category.
+    if (matches === null || matches.length != 2) {
+        return Category.Other;
+    }
+
+    const text = matches[1].toLowerCase();
+
+    return CATEGORIES.find(category => category.toLowerCase() === text)
+        || CATEGORY_MAPPINGS.get(text)
+        || Category.Other;
+}
